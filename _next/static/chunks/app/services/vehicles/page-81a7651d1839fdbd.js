a7cda2fc90fbef7a5a8a900b06c2b9700d22c0fc
masihtehrani <masihtehrani@users.noreply.github.com>
2025-10-04 07:22:35 +0000

    deploy: 3f4c33223691aa2874d9873ecec1a4f62030a180

---
+++ _next/static/chunks/app/services/vehicles/page-81a7651d1839fdbd.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[108,220,281,325,345,352,529,669,763,807,983],{1404:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,2619,23))}},e=>{e.O(0,[619,441,255,358],()=>e(e.s=1404)),_N_E=e.O()}]);
